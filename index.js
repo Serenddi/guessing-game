d7d7553478178190a7e6d72f483be4388846fea5
Anastasiia Mikhaleva <dadanastya24342@gmail.com>
2022-09-02 23:14:20 +0700

    feat: do reset function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let number_place = document.querySelector('.number_place')
 let message = document.querySelector('.message');
 const check = document.querySelector('.check_btn')
 const reset = document.querySelector('.reset_btn')
+let score = 20;
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1)
 
@@ -15,22 +16,37 @@ function guessNumber() {
   if (guess_number.value == '') {
     message.textContent = 'Not a number!'
   }
-  else if (guess_number.value < secretNumber) {
+  else if (guess_number.value < secretNumber && score > 0) {
     message.textContent = "Too low!"
+    score--;
+    document.querySelector('.score').textContent = `Score: ${score}`;
+
+    if (score === 0) {
+      message.textContent = "You lost :("
+    }
   }
-  else if (guess_number.value > secretNumber) {
+  else if (guess_number.value > secretNumber && score > 0) {
     message.textContent = "Too high!"
+    score--;
+    document.querySelector('.score').textContent = `Score: ${score}`;
+
+    if (score === 0) {
+      message.textContent = "You lost :("
+    }
   }
   else if (guess_number.value == secretNumber) {
     message.textContent = "You won!"
     number_place.textContent = secretNumber;
-    document.body.classList.add('win');
+    document.body.style.background = 'rgb(46, 159, 46)';
   }
 }
 
 function resetGame() {
-  secretNumber;
+  score = 20;
+  document.querySelector('.score').textContent = `Score: ${score}`;
+  secretNumber = Math.trunc(Math.random() * 20 + 1)
   guess_number.value = '';
+  number_place.textContent = '?';
   message.textContent = 'Start guessing:)'
-  document.body.style.background = '#FFFFF';
+  document.body.style.background = '#000000'
 }
